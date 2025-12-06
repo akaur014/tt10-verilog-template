81b52a10d96712883f16a3e3e5a9bf309c7ca6f2
akaur014 <ia0111@wayne.edu>
2025-12-06 18:00:00 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -4,7 +4,7 @@
  */
 
 `default_nettype none
-module tt_um_akaur_nand (
+module tt_um_akaur014_nand (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
